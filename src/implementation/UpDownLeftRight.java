0b8d1478c31f166aae9e99d21ba32a91bdcfa068
woozink <k0109974@naver.com>
2022-02-14 17:48:47 +0900

    구현 파트 예제문제

---
+++ src/implementation/UpDownLeftRight.java
@@ -0,0 +1,45 @@
+package implementation;
+
+import java.util.Scanner;
+
+public class UpDownLeftRight {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        //N을 입력 받기
+
+        int n = sc.nextInt();
+        sc. nextLine();
+        String[] plans = sc. nextLine().split(" ");
+        int x= 1;
+        int y = 1;
+
+
+        // L R U D에 따른 이동 방향
+
+        int[] dx = {0,0,-1,1,};
+        int[] dy = {-1,-1,0,0};
+        char[] moveTypes = {'L' , 'R', 'U', 'D'};
+
+        //이동 계획을 하나씩 확인
+        for (int i = 0; i < plans.length;  i++){
+            char plan = plans[i].charAt(0);
+            // 이동 후 좌표구하기
+            int nx = -1;
+            int ny = -1;
+            for(int j =0; j < 4; j++){
+                if(plan == moveTypes[j]){
+                    nx = x + dx[j];
+                    ny = y + dy[j];
+                }
+            }
+            if(nx < 1 || ny < 1 || nx > n || ny > n){
+                continue;
+            }
+            x = nx;
+            y = ny;
+
+        }
+        System.out.println(x + " " + y);
+    }
+}
