55625cf0c2a26e00ffd5e61a242f2d8233a4726d
woozink <k0109974@naver.com>
2022-02-18 16:55:57 +0900

    구현 뱀문제

---
+++ src/implementation/Snake.java
@@ -0,0 +1,127 @@
+package implementation;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+class Node {
+    private int time;
+    private char direction;
+
+    public Node(int time, char direction) {
+        this.time = time;
+        this.direction = direction;
+    }
+
+    public int getTime() {
+        return this.time;
+    }
+
+    public char getDirection() {
+        return direction;
+    }
+}
+
+class Position {
+
+    private int x;
+    private int y;
+
+    public Position(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return this.x;
+    }
+
+    public int getY() {
+        return this.y;
+    }
+
+}
+
+public class Snake {
+
+    public static int n, k, l;
+    public static int[][] arr = new int[101][101];
+    public static ArrayList<Node> info = new ArrayList<>();
+
+    public static int dx[] = {0, 1, 0, -1};
+    public static int dy[] = {1, 0, -1, 0};
+
+    public static int turn(int direction, char c) {
+        if (c == 'L') direction = (direction == 0) ? 3 : direction - 1;
+        return direction;
+    }
+
+    public static int simulate() {
+        int x = 1, y = 1;
+        arr[x][y] = 2;
+        int direction = 0;
+        int time = 0;
+        int index = 0;
+        Queue<Position> q = new LinkedList<>();
+        q.offer(new Position(x, y));
+
+        while (true) {
+            int nx = x + dx[direction];
+            int ny = y + dy[direction];
+
+            if (1 <= nx && nx <= n && 1 <= ny && arr[nx][ny] != 2) {
+
+                if (arr[nx][ny] == 0) {
+                    arr[nx][ny] = 2;
+                    q.offer(new Position(nx, ny));
+                    Position prev = q.poll();
+                    arr[prev.getX()][prev.getY()] = 0;
+                }
+                if (arr[nx][ny] == 1) {
+                    arr[nx][ny] = 2;
+                    q.offer(new Position(nx, ny));
+                }
+            } else {
+                time += 1;
+                break;
+            }
+            x = nx;
+            y = ny;
+            time += 1;
+            if (index < l && time == info.get(index).getTime()) {
+                direction = turn(direction, info.get(index).getDirection());
+            }
+        }
+        return time;
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        k = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        for (int i = 0; i < k; i++) {
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            arr[a][b] = 1;
+        }
+        l = Integer.parseInt(br.readLine());
+        for (int i = 0; i < l; i++) {
+            int x = Integer.parseInt(st.nextToken());
+            char c = br.readLine().charAt(0);
+
+            info.add(new Node(x, c));
+
+        }
+        System.out.println(simulate());
+    }
+
+
+}
+
--- src/implementation/StringCompression.java
+++ src/implementation/StringCompression.java
@@ -1,34 +1,34 @@
-package implementation;
-
-public class StringCompression {
-    public int Solution (String s ) {
-        int answer = s.length();
-        // 1개 단위 부터 압축 단위를 늘려가며 확인
-        for(int step = 1; step<s.length()/2+1; step++){
-            String compressed = "";
-            String prev = s.substring(0,step);
-            int cnt = 1;
-            //단위 크기 만큼 증가시키ㅕ 이전 문자열과 비교
-            for(int j = step; j < s.length(); j+=step){
-                //이전 상태와 동일하다면 압축 횟수 증가
-                String sub = "";
-                for(int k = j; k<j+step; k++){
-                    if(k < s.length()) sub += s.charAt(k);
-                    if(prev.equals(sub)) cnt++;
-                    else{
-                        compressed += (cnt <=2) ? cnt +prev : prev;
-                        sub = "";
-                        for (int k = j; k < j + step; k++) {
-                            if(k < s.length()) sub += s.charAt(k);
-
-                        }
-                    }
-                    prev = sub;
-                    cnt =1;
-                }
-                compressed += (cnt >= 2) ? cnt + prev : prev;
-                answer = Math.min(answer, compressed.length());
-            }
-        }
-    }
-}
+//package implementation;
+//
+//public class StringCompression {
+//    public int Solution (String s ) {
+//        int answer = s.length();
+//        // 1개 단위 부터 압축 단위를 늘려가며 확인
+//        for(int step = 1; step<s.length()/2+1; step++){
+//            String compressed = "";
+//            String prev = s.substring(0,step);
+//            int cnt = 1;
+//            //단위 크기 만큼 증가시키ㅕ 이전 문자열과 비교
+//            for(int j = step; j < s.length(); j+=step){
+//                //이전 상태와 동일하다면 압축 횟수 증가
+//                String sub = "";
+//                for(int k = j; k<j+step; k++){
+//                    if(k < s.length()) sub += s.charAt(k);
+//                    if(prev.equals(sub)) cnt++;
+//                    else{
+//                        compressed += (cnt <=2) ? cnt +prev : prev;
+//                        sub = "";
+//                        for (int k = j; k < j + step; k++) {
+//                            if(k < s.length()) sub += s.charAt(k);
+//
+//                        }
+//                    }
+//                    prev = sub;
+//                    cnt =1;
+//                }
+//                compressed += (cnt >= 2) ? cnt + prev : prev;
+//                answer = Math.min(answer, compressed.length());
+//            }
+//        }
+//    }
+//}
